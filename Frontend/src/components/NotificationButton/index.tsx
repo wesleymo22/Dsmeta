7888232ccc1583122e0abba821db0fb6bf0acf39
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-09-09 15:21:12 -0300

    Send Notification

--- Frontend/src/components/NotificationButton/index.tsx
+++ Frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+
+}
+
+function handleclick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("sucesso")
+    })
+}
+
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleclick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
